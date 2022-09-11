7e48c925a3e653a4d6807f86e539c1e0274511aa
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-11 23:58:01 +0400

    sq-ch5-ex1 proves that singleton scope beans aren't multythread safety

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,14 +2,36 @@ package main;
 
 import config.ProjectConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import repositories.CommentRepository;
 import services.CommentService;
 import services.UserService;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         var c = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
         var s1 = c.getBean(CommentService.class);
         var s2 = c.getBean(UserService.class);
-        System.out.println(s1.getCommentRepository() == s2.getCommentRepository());
+
+        Thread thread1 = new Thread(() -> {
+            CommentRepository commentRepository = s1.getCommentRepository();
+            for (int i = 0; i < 1000; i++) {
+                commentRepository.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            CommentRepository commentRepository = s2.getCommentRepository();
+            for (int i = 0; i < 1000; i++) {
+                commentRepository.decrement();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(s1.getCommentRepository().getCount());
     }
 }
--- src/main/java/repositories/CommentRepository.java
+++ src/main/java/repositories/CommentRepository.java
@@ -4,4 +4,18 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class CommentRepository {
+
+    private int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }

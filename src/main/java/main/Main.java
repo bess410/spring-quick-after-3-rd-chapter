b871338de9178e710f2d320ce417fc4968a0f1a6
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-12 00:03:15 +0400

    sq-ch5-ex3 eager initialization

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,36 +2,9 @@ package main;
 
 import config.ProjectConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import repositories.CommentRepository;
-import services.CommentService;
-import services.UserService;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        var c = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
-        var s1 = c.getBean(CommentService.class);
-        var s2 = c.getBean(UserService.class);
-
-        Thread thread1 = new Thread(() -> {
-            CommentRepository commentRepository = s1.getCommentRepository();
-            for (int i = 0; i < 1000; i++) {
-                commentRepository.increment();
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            CommentRepository commentRepository = s2.getCommentRepository();
-            for (int i = 0; i < 1000; i++) {
-                commentRepository.decrement();
-            }
-        });
-
-        thread1.start();
-        thread2.start();
-
-        thread1.join();
-        thread2.join();
-
-        System.out.println(s1.getCommentRepository().getCount());
+    public static void main(String[] args) {
+        new AnnotationConfigApplicationContext(ProjectConfiguration.class);
     }
 }

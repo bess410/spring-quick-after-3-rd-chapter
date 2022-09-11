ff69b9588d28418ce45aa43e04185d80710a3673
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-11 14:03:17 +0400

    sq-ch4-ex2 Autowiring by Spring

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -2,10 +2,12 @@ package services;
 
 import lombok.Data;
 import model.Comment;
+import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 
 @Data
+@Component
 public class CommentService {
 
     private final CommentRepository commentRepository;

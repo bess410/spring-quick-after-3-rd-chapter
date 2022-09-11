b83e279dcc9e780fbca21ad36be7a9323fbd3736
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-12 00:16:17 +0400

    sq-ch5-ex5 prototype scope proves

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,8 +7,10 @@ import services.CommentService;
 public class Main {
     public static void main(String[] args) {
         var c = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
-        System.out.println("Before retrieving the CommentService");
-        c.getBean(CommentService.class);
-        System.out.println("After retrieving the CommentService");
+
+        var cs1 = c.getBean("commentService", CommentService.class);
+        var cs2 = c.getBean("commentService", CommentService.class);
+
+        System.out.println(cs1 == cs2);
     }
 }

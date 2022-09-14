6afa7c261281de4f51094dec88539896481d3069
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-14 19:12:38 +0400

    sq-ch6-ex5 using @AfterReturning

--- src/main/java/aspects/LoggingAspect.java
+++ src/main/java/aspects/LoggingAspect.java
@@ -1,6 +1,7 @@
 package aspects;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -13,19 +14,8 @@ import java.util.logging.Logger;
 public class LoggingAspect {
     private final Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
-    @Around("@annotation(annotations.ToLog)")
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        String methodName = joinPoint.getSignature().getName();
-        Object[] args = joinPoint.getArgs();
-
-        logger.info("Method " + methodName +
-                " with parameters " + Arrays.asList(args) +
-                " will execute");
-
-        Object returnedByMethod = joinPoint.proceed(args);
-
-        logger.info("Method executed and returned " + returnedByMethod);
-
-        return returnedByMethod;
+    @AfterReturning(value = "@annotation(annotations.ToLog)", returning = "returnedValue")
+    public void log(Object returnedValue) {
+        logger.info("Method executed and returned " + returnedValue);
     }
 }
